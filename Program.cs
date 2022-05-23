6fd64dbbb008d903230635b42b4ae21a8ac87ace
nicklegr <1327502+nicklegr@users.noreply.github.com>
2022-05-24 00:53:38 +0900

    ビルド通った

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using Vosk;
 
 namespace SpeechRecognitionVosk
 {
     class Program
     {
+        public static void DemoBytes(Model model)
+        {
+            // Demo byte buffer
+            VoskRecognizer rec = new VoskRecognizer(model, 16000.0f);
+            rec.SetMaxAlternatives(0);
+            rec.SetWords(true);
+            using (var source = File.OpenRead("test.wav"))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    if (rec.AcceptWaveform(buffer, bytesRead))
+                    {
+                        Console.WriteLine(rec.Result());
+                    }
+                    else
+                    {
+                        Console.WriteLine(rec.PartialResult());
+                    }
+                }
+            }
+            Console.WriteLine(rec.FinalResult());
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // You can set to -1 to disable logging messages
+            Vosk.Vosk.SetLogLevel(0);
+
+            Model model = new Model("model");
+            DemoBytes(model);
         }
     }
 }
